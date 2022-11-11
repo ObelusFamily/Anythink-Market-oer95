95efe4c7bed273b0131913c71bb072eaa86b3a8c
Lucas <lucas.kiers@eclipso.at>
2022-11-11 22:21:22 +0100

    Revert "Added a Search filter to the Home screen to filter throught all Items"
    This reverts commit 53cd4e1092cf93e7bb4a37e5663d813f641731fe.

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,32 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import logo from "../../imgs/logo.png";
-import agent from "../../agent";
 
-const Banner = (props) => {
-
-  useEffect(() => {
-    console.log(props)
-  }, [])
-
-  function onInput(e) {
-    let value = e.target.value;
-      props.onLoad(
-        "all",
-        value.length >= 3 ? agent.Items.byTitle(value) : agent.Items.all(),
-        Promise.all([agent.Tags.getAll(), value.length >= 3 ? agent.Items.byTitle(value) : agent.Items.all()])
-      );
-  }
-
+const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
-          <div id="search-container">
-          <input type="search" placeholder="What is it that you truly desire?" id="search-box" onInput={onInput} />
-          <span className="icon">🔍</span>
-          </div>
           <span> the cool stuff.</span>
         </div>
       </div>
