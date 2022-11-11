53cd4e1092cf93e7bb4a37e5663d813f641731fe
Lucas <lucas.kiers@eclipso.at>
2022-11-11 22:14:51 +0100

    Added a Search filter to the Home screen to filter throught all Items

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -45,7 +45,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-page">
-        <Banner />
+        <Banner onLoad={this.props.onLoad} />
 
         <div className="container page">
           <Tags tags={this.props.tags} onClickTag={this.props.onClickTag} />
